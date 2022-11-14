936a4fa755ec428fadf751132e576fe63d7b8d3b
DushanSenadheera <you@example.com>
2022-11-14 12:48:57 +0530

    add files

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -1,18 +1,19 @@
 import express from "express";
+import {MongoClient} from 'mongodb';
 
-let articlesInfo = [{
-    name: 'learn-react',
-    upvotes:0,
-    comments: [],
-},{
-    name: 'learn-node',
-    upvotes: 0,
-    comments: [],
-},{
-    name: 'mongodb',
-    upvotes: 0,
-    comments: [],
-}]
+// let articlesInfo = [{
+//     name: 'learn-react',
+//     upvotes:0,
+//     comments: [],
+// },{
+//     name: 'learn-node',
+//     upvotes: 0,
+//     comments: [],
+// },{
+//     name: 'mongodb',
+//     upvotes: 0,
+//     comments: [],
+// }]
 
 const app = express();
 app.use(express.json());
@@ -25,6 +26,25 @@ app.use(express.json());
 //     const {name} = req.params;
 //     res.send(`Hello ${name}!!`);
 // })
+
+app.get('/api/articles/:name', async (req, res)=>{
+    const {name} = req.params;
+
+    const client = new MongoClient('mongodb://127.0.0.1:27017')
+    await client.connect();
+
+    const db = client.db('react-blog-db');
+
+    const article = await db.collection('articles').findOne({name});
+
+    if(article){
+        res.json(article);
+    }
+    else{
+        res.sendStatus(404);
+    }
+    
+});
 
 app.put('/api/articles/:name/upvote', (req, res)=>{
     const {name} = req.params;
